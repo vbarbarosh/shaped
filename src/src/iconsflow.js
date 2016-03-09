dbd915817d88df3003216cc5fcc2aefce195f1db
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2016-03-09 21:49:59 +0200

    progress

--- src/src/iconsflow.js
+++ src/src/iconsflow.js
@@ -9,6 +9,29 @@ jQuery(function () {
         },
         preview = jQuery('#gallery > div:nth-child(2)');
 
+    function search(term, page, pageSize)
+    {
+        if (ajax) {
+            ajax.abort();
+            ajax = null;
+        }
+
+        return Promise.resolve(cache.search[term] ? cache.search[term] : ajax = jQuery.ajax(searchUrl, {data: {term: term}}))
+            .then(function (searchResponse) {
+                var offset = (page - 1) * pageSize;
+                cache.search[term] = searchResponse;
+                ajax = jQuery.ajax(fetchUrl, {data: {icons: searchResponse.icons.slice(offset, offset + pageSize)}});
+                return [searchResponse.icons.length, Promise.resolve(ajax)];
+            })
+            .spread(function (found, fetchResponse) {
+                var key, fetched = [];
+                for (key in fetchResponse) {
+                    fetched.push('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100">' + fetchResponse[key].defs[0] + '</svg>');
+                }
+                return [found, fetched];
+            });
+    }
+
     Vue.filter('svgInline', function (svg) {
         return 'data:image/svg+xml,' + svg;
     });
@@ -17,51 +40,41 @@ jQuery(function () {
         el: '#gallery',
         data: {
             term: 'animal',
+            searching: false,
             page: 1,
             pageSize: 1,
+            found: 1,
             icons: []
-        },
-        computed: {
-            searching: function () {
-                return ajax !== null;
-            }
         },
         methods: {
             prev: function () {
                 if (this.page > 1) {
-                    this.page -= 1;
+                    this.page = parseInt(this.page, 10) - 1;
                 }
+                this.search();
             },
             next: function () {
-                if ((this.page + 1) * this.pageSize < this.allIcons.length) {
-                    this.page += 1;
+                var last = Math.ceil(this.found / this.pageSize);
+                if (this.page + 1 <= last) {
+                    this.page = parseInt(this.page, 10) + 1;
                 }
+                this.search();
             },
             refreshPageSize: function () {
                 var pageSize = Math.floor(preview.height() / (preview.width() / 3))*3;
                 if (this.pageSize != pageSize) {
                     this.pageSize = pageSize;
                     this.page = 1;
+                    this.search();
                 }
             },
             search: function () {
-                var term = this.term,
-                    _this = this;
-                if (ajax) {
-                    ajax.abort();
-                    ajax = null;
-                }
-                Promise.resolve(cache.search[term] ? cache.search[term] : ajax = jQuery.ajax(searchUrl, {data: {term: term}}))
-                    .then(function (searchResponse) {
-                        cache.search[term] = searchResponse;
-                        return Promise.resolve(ajax = jQuery.ajax(fetchUrl, {data: {icons: searchResponse.icons.slice(0, 10)}}));
-                    })
-                    .then(function (fetchResponse) {
-                        var key, icons = [];
-                        for (key in fetchResponse) {
-                            icons.push('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100">' + fetchResponse[key].defs[0] + '</svg>');
-                        }
-                        _this.icons = icons;
+                var _this = this;
+                this.searching += 1;
+                search(this.term, this.page, this.pageSize)
+                    .spread(function (found, fetched) {
+                        _this.found = found;
+                        _this.icons = fetched;
                     })
                     .catch(function (error) {
                         // Ignore ajax.abort() calls
@@ -70,7 +83,7 @@ jQuery(function () {
                         }
                     })
                     .finally(function () {
-                        ajax = null;
+                        _this.searching -= 1;
                     });
             }
         },
